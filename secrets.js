9fb7f1a1e5e57a35ef9c2cd067620c4fdcbcb8c5
Iskak Mantyubetov <ayzik55@gmail.com>
2020-07-08 12:45:31 -0400

    added plaid login

---
+++ secrets.js
@@ -0,0 +1,4 @@
+process.env.PLAID_CLIENT_ID = '5efb9f1c11b5dd0012208f76'
+process.env.PLAID_SECRET = '8088aa369930fb15dc1c71117829f5'
+process.env.PLAID_PUBLIC_KEY = 'ae9b699cddb974bc89c10074b92e85'
+module.exports = process.env
